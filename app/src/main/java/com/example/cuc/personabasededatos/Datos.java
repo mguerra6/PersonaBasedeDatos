1b72e05a8aed4a4eff0352495e68007cea1343cb
Marlon Guerra Mulford <mguerramulford@gmail.com>
2017-05-13 11:58:30 -0500

    Interfaces termiadas, base de datos terminada y lista principal termianda, cuadrando regsitro, cargando los datos en tabla listado

---
+++ app/src/main/java/com/example/cuc/personabasededatos/Datos.java
@@ -0,0 +1,52 @@
+package com.example.cuc.personabasededatos;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+/**
+ * Created by CUC on 13/05/2017.
+ */
+
+public class Datos {
+    public static ArrayList<Persona> traerPersona(Context contexto)
+    {
+        ArrayList<Persona> persona = new ArrayList<>();
+
+        //Declarar varieables
+        SQLiteDatabase db;
+        String sql, foto, cedula, nombre, apellido, sexo, pasatiempo;
+        Persona p;
+
+        //Abrir conexion
+        PersonasSQLliteOpenhelper aux = new PersonasSQLliteOpenhelper(contexto, "DBPersonas", null, 1);
+        db = aux.getReadableDatabase();
+
+        //Cursor
+        sql = "select * from persona";
+        Cursor c = db.rawQuery(sql, null);
+
+
+        //Recorrer el cursor
+        if(c.moveToFirst()){
+            do
+            {
+                foto = c.getString(0);
+                cedula = c.getString(1);
+                nombre = c.getString(2);
+                apellido = c.getString(3);
+                sexo = c.getString(4);
+                pasatiempo = c.getString(5);
+
+                p = new Persona(foto, cedula, nombre, apellido, sexo, pasatiempo);
+                persona.add(p);
+
+            }while(c.moveToNext());
+        }
+
+        db.close();
+        return persona;
+    }
+}
--- app/src/main/java/com/example/cuc/personabasededatos/Listado_tablas.java
+++ app/src/main/java/com/example/cuc/personabasededatos/Listado_tablas.java
@@ -2,12 +2,22 @@ package com.example.cuc.personabasededatos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
 
 public class Listado_tablas extends AppCompatActivity {
+
+    private TableLayout tabla;
+    private ArrayList<Persona> personas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_tablas);
+
+        tabla = (TableLayout)findViewById(R.id.tablaPersonas);
+        personas = Datos.traerPersona(getApplicationContext());
     }
 }
